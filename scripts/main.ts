141b72a503b7e2b2b40c51b43b65783bc0eb4183
Pochang Lee <pochanglee2008@gmail.com>
2023-03-29 17:09:29 +0800

    capture word

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,14 +1,20 @@
 let selection: string = ''
 let updated = false
 
-document.addEventListener('selectionchange', () => {
-  selection = getSelection().toString()
+document.addEventListener('selectionchange', (event) => {
+  selection = getSelection().toString().trim()
   updated = true
 })
 
-document.addEventListener('mouseup', () => {
-  if (updated) {
-    alert(`${selection} selected`)
+function isWord(str: string): boolean {
+  return str.toLowerCase().search(/^[a-z]+$/) === 0
+}
+
+document.addEventListener('mouseup', async (event: {clientX: number, clientY: number}) => {
+  if (updated && selection && isWord(selection)) {
+    selection.trim().toLowerCase()
+    window.console.log(chrome)
+    await chrome.action.openPopup()
   }
   updated = false
 })
